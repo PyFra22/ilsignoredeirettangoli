801b483bd4eb4266f0f7f36fd08f99aa3338b4f6
PyFra22 <locchifrancescoo@gmail.com>
2024-05-25 19:26:19 +0200

    Update rectcollision.py
    Modifica dei tasti per l'uscita dal gioco e per creare una nuova partita, aggiunto il rettangolo in basso in grado di eliminare il giocatore

--- rectcollision.py
+++ rectcollision.py
@@ -40,14 +40,12 @@ main_rectangle = pyg.Rect(0, 0, 30, 30)
 font_quit_button = pyg.font.SysFont("Arial", 24)
 text_quit_button = font_quit_button.render("QUIT", True, color_white)
 text_quit_rect = text_quit_button.get_rect()
-text_quit_rect.center = (width/1.6, height/2)
-
-font_retry_button = pyg.font.SysFont("Arial", 24)
-text_retry_button = font_retry_button.render("TRY AGAIN", True, color_white)
-text_retry_rect = text_retry_button.get_rect()
-text_retry_rect.center = (width/2.6, height/2)
+text_quit_rect.center = (width/2, height/2)
 
 pyg.mouse.set_visible(False)
+
+border_outside_rectangle = pyg.Rect(0, 300 , 800, 125)
+mouse_pace = 0.5
 
 while running:
     
@@ -69,6 +67,10 @@ while running:
     window.blit(text_number_of_life, textRect_number_of_life)           
     window.blit(text_life, textRect_tot_life)
 
+    pyg.draw.rect(window, color_black, border_outside_rectangle)
+    if main_rectangle.colliderect(border_outside_rectangle) and total_live != 0:
+        total_live -= 1
+        rectangle_color = color_red
     
     for obstacle in obstacles:
         pyg.draw.rect(window, color_blue, obstacle)
@@ -78,7 +80,7 @@ while running:
     main_rectangle.center = position_of_mouse
 
     pyg.draw.rect(window, rectangle_color, main_rectangle)
-
+            
     if total_live == 0:
         window.fill("black")
         font_text_lose = pyg.font.Font('freesansbold.ttf', 48)
@@ -88,20 +90,22 @@ while running:
         window.blit(text_lose, textRect_lose)
         pyg.mouse.set_visible(True)
         window.blit(text_quit_button, text_quit_rect)
-        window.blit(text_retry_button, text_retry_rect)
+        
+    retry_button = pyg.key.get_pressed()
+    exit_button = pyg.key.get_pressed()
+        
+    if retry_button[pyg.K_r] and total_live == 0:
+            total_live = 100
+            obstacles = []             
+            for _ in range(45):
+                    obstacle_rectangle = pyg.Rect(rm.randint(0, 600), rm.randint(0, 250), 25, 25)
+                    obstacles.append(obstacle_rectangle)
+    if exit_button[pyg.K_ESCAPE]:
+        running = False
         
     for event in pyg.event.get():
         if event.type == pyg.QUIT:
-            running = False   
-        if event.type == pyg.MOUSEBUTTONDOWN:
-            if width/2.6 <= position_of_mouse[0] <= width/2.6+100 and height/2 <= position_of_mouse[1] <= height/2+35: 
-                total_live = 100
-                obstacles = []
-                for _ in range(45):
-                        obstacle_rectangle = pyg.Rect(rm.randint(0, 600), rm.randint(0, 250), 25, 25)
-                        obstacles.append(obstacle_rectangle)
-            if width/1.6 <= position_of_mouse[0] <= width/1.6+100 and height/2 <= position_of_mouse[1] <= height/2+35:
-                running = False
+            running = False
         
     pyg.display.flip()
     dt = clock.tick(60)/1000
